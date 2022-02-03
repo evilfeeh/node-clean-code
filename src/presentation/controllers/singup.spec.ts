f8627cf82168633b82d594589017efdad34e50a4
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2022-02-03 13:13:14 -0300

    test: ensure SingUpController returns 400 if no password is provided

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -31,4 +31,19 @@ describe('singUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
+
+  test('Should return 400 if no password is provided', () => {
+    const sut = new SingUpController()
+    const httpRequest = {
+      body: {
+        name: 'fulaninhous fulano',
+        email: 'teste@gmail.com',
+        passwordConfirm: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
 })

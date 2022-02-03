ab4399594c58591f350f7874a3f92af2d8aab4a3
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2022-02-03 17:26:05 -0300

    test: ensure SingUpController calls emailValidator with the correct Email

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -95,8 +95,22 @@ describe('singUp Controller', () => {
       }
     }
     const httpResponse = sut.handle(httpRequest)
-
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
+  })
+
+  test('Should call EmailValidator with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    const httpRequest = {
+      body: {
+        name: 'fulaninhous fulano',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
